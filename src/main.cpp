e8e25bd4704c9a4f9516732a9779f941668cc4cf
Mihir Laud <mihirlaud@gmail.com>
2022-02-20 16:56:15 -0500

    Create PROS template

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include "main.h"
+
+void initialize() {
+}
+
+void disabled() {
+}
+
+void competition_initialize() {
+}
+
+void autonomous() {
+}
+
+void opcontrol() {
+
+	while (true) {
+		pros::delay(20);
+	}
+}
